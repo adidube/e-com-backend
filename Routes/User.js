7f842c7dd093622e9cd3b1a13e69b6d1e80be32e
adidube <57318272+adidube@users.noreply.github.com>
2025-08-24 15:04:14 +0530

    Update User.js

--- Routes/User.js
+++ Routes/User.js
@@ -15,6 +15,7 @@ router.get('/users',userController.users)
 router.get("/profile", Auth.Authenticated, userController.profile);
 
 router.post('/webhook',(req,res)=>{
+    console.info(req.body);
     res.status(200).json({
         status:"success",
         data:req.body
